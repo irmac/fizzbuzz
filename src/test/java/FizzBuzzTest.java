614b76c15e8151e77cd14c32643cce4c271668df
Iain MacLeod <irmac@me.com>
2023-12-11 19:06:23 +0000

    initial commit

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,15 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    @Test
+    void shouldReturnFizzFor3() {
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(3));
+    }
+
+
+
+}
