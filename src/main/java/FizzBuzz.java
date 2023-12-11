ce619a2bb5ad5b7d09790dc452285a9c63ac0e29
LidzDev <lydia.cordell@gmail.com>
2023-12-11 20:22:58 +0000

    tidied comments

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,17 +3,19 @@ public class FizzBuzz {
     }
 
     public static String fizzBuzz(int inputNumber) {
+//      if divisible by 5 and 3 its FizzBuzz
         if ((inputNumber %5 == 0) && (inputNumber %3 ==0)) {
-//       deal with multiples of 5
             return "FizzBuzz";
         }
+//      if divisible by 3 but not 5 its Fizz
         else if (inputNumber %3 == 0) {
-//      deal with multiples of three
             return "Fizz";
-        } else if (inputNumber %5 == 0){
-//       deal with multiples of 5
+        }
+//      if divisible by 5 but not 3 its Buzz
+        else if (inputNumber %5 == 0){
             return "Buzz";
         }
+//      otherwise it should return i
         else {
             return "i";
         }
