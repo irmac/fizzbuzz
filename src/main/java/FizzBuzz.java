c9b143d24a4c12b065c67c2699c4a34eda8b6b7d
LidzDev <lydia.cordell@gmail.com>
2023-12-12 12:08:27 +0000

    refactored fizzbuzz conditional structure

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,24 +3,21 @@ public class FizzBuzz {
     }
 
     public static String fizzBuzz(int inputNumber) {
-//      if divisible by 5 and 3 its FizzBuzz
+
         if (isDivisibleBy5And3(inputNumber)) {
             return "FizzBuzz";
         }
-//      if divisible by 3 but not 5 its Fizz
-        else if (isDivisibleBy3(inputNumber)) {
+
+        if (isDivisibleBy3(inputNumber)) {
             return "Fizz";
         }
-//      if divisible by 5 but not 3 its Buzz
-        else if (isDivisibleBy5(inputNumber)){
+
+        if (isDivisibleBy5(inputNumber)){
             return "Buzz";
         }
-//      otherwise it should return i
-        else {
-            return "i";
-        }
 
-
+        return "i";
+        
 
     }
 
